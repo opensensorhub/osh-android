9b966d23d6239412f42c321ef260d93010634f7f
Joshua Wolfe <developer.wolfe@gmail.com>
2019-03-07 15:38:41 -0600

    WIP to fix cloning/crashing/stackoverflow error

---
+++ sensorhub-android-app/src/org/sensorhub/android/SOSServiceWithIPCConfig.java
@@ -0,0 +1,14 @@
+package org.sensorhub.android;
+
+import android.content.Context;
+
+import org.sensorhub.impl.service.sos.SOSServiceConfig;
+
+public class SOSServiceWithIPCConfig extends SOSServiceConfig
+{
+    Context androidContext;
+
+    public SOSServiceWithIPCConfig() {
+        super();
+    }
+}
