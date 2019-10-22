be64cb368409c80b6303979488574d0a16f6214c
Ian Patterson <cr31.dev@gmail.com>
2019-10-22 13:16:39 -0500

    Fixed another conversion issue caused by incorrect LLA values Remove unused code

--- sensorhub-android-blebeacon/src/main/java/org/sensorhub/impl/sensor/blebeacon/BLEBeaconDriver.java
+++ sensorhub-android-blebeacon/src/main/java/org/sensorhub/impl/sensor/blebeacon/BLEBeaconDriver.java
@@ -9,8 +9,6 @@ import android.os.RemoteException;
 import android.provider.Settings;
 import android.util.Log;
 
-import net.opengis.sensorml.v20.PhysicalComponent;
-
 import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
@@ -18,10 +16,9 @@ import org.altbeacon.beacon.BeaconParser;
 import org.altbeacon.beacon.RangeNotifier;
 import org.altbeacon.beacon.Region;
 import org.altbeacon.beacon.utils.UrlBeaconUrlCompressor;
-import org.sensorhub.algo.geoloc.NadirPointing;
+import org.sensorhub.algo.geoloc.GeoTransforms;
 import org.sensorhub.algo.vecmath.Mat3d;
 import org.sensorhub.algo.vecmath.Vect3d;
-import org.sensorhub.algo.geoloc.GeoTransforms;
 import org.sensorhub.api.common.SensorHubException;
 import org.sensorhub.api.sensor.ISensorDataInterface;
 import org.sensorhub.impl.sensor.AbstractSensorModule;
@@ -40,40 +37,23 @@ public class BLEBeaconDriver extends AbstractSensorModule<BLEBeaconConfig> imple
     private static final String TAG = "BLEBeaconDriver";
 
     String localFrameURI;
-    List<PhysicalComponent> smlComponents;
+    //    List<PhysicalComponent> smlComponents;
     private BeaconManager mBeaconManager;
     private BLEBeaconRawOutput rawOutput;
     private BLEBeaconLocationOutput locOutput;
     private List<Beacon> closestBeacons;
-//    private Map<String, double[]> urlLocations;
     private Map<String, Vect3d> url2Locations;
-    private Map<String, double[]> urlLocationsCart;
     private Map<String, Beacon> beaconMap;
     private Triangulation triangulation;
 
     public BLEBeaconDriver() {
         // TODO: implement something better for this down the road
         beaconMap = new HashMap<>();
-//        urlLocations = new HashMap<>();
         url2Locations = new HashMap<>();
-//        urlLocationsCart = new HashMap<>();
 
-//        urlLocations.put("http://rpi4-1", new double[]{34.2520736, -86.2012028, 283.0});
-//        urlLocations.put("http://opensensorhub.org", new double[]{34.2520221, -86.2012018, 283.0});
-//        urlLocations.put("http://cardbeacon", new double[]{34.2520761, -86.2012561, 283.0});
-
-        // LON, LAT, ALT
-        /*url2Locations.put("http://rpi4-1", new Vect3d( -86.2012028,34.2520736, 283.0));
-        url2Locations.put("http://opensensorhub.org", new Vect3d( -86.2012018,34.2520221, 283.0));
-        url2Locations.put("http://cardbeacon", new Vect3d(-86.2012561,34.2520761,  283.0));*/
-        url2Locations.put("http://rpi4-1", new Vect3d( -86.2012028 * Math.PI/180,34.2520736 * Math.PI/180, 283.0 * Math.PI/180));
-        url2Locations.put("http://opensensorhub.org", new Vect3d( -86.2012018 * Math.PI/180,34.2520221 * Math.PI/180, 283.0 * Math.PI/180));
-        url2Locations.put("http://cardbeacon", new Vect3d(-86.2012561 * Math.PI/180,34.2520761 * Math.PI/180,  283.0 * Math.PI/180));
-
-
-//        urlLocationsCart.put("http://rpi4-1", new double[]{349670.211399502, -5266209.53754881, 3569752.24484278});
-//        urlLocationsCart.put("http://opensensorhub.org", new double[]{349670.516346565, -5266212.73984997, 3569752.24484278});
-//        urlLocationsCart.put("http://cardbeacon", new double[]{349665.302111348, -5266209.70708302, 3569752.24484278});
+        url2Locations.put("http://rpi4-1", new Vect3d(-86.2012028 * Math.PI / 180, 34.2520736 * Math.PI / 180, 283.0));
+        url2Locations.put("http://opensensorhub.org", new Vect3d(-86.2012018 * Math.PI / 180, 34.2520221 * Math.PI / 180, 283.0));
+        url2Locations.put("http://cardbeacon", new Vect3d(-86.2012561 * Math.PI / 180, 34.2520761 * Math.PI / 180, 283.0));
     }
 
     @Override
@@ -167,7 +147,6 @@ public class BLEBeaconDriver extends AbstractSensorModule<BLEBeaconConfig> imple
                 Log.d(TAG, "Beacon ID: " + beacon.getId1() + "\nBeacon URL: " + url +
                         " approximately " + beacon.getDistance() + " meters away.");
                 // TODO: Need to improve this to handle non-EsURL beacons that have info in the other ID slots
-//                this.beacons.put(beacon.getId1(), beacon);
                 rawOutput.sendBeaconRecord(beacon);
                 if (closestBeacon == null || beacon.getDistance() < closestBeacon.getDistance()) {
                     closestBeacon = beacon;
@@ -184,14 +163,10 @@ public class BLEBeaconDriver extends AbstractSensorModule<BLEBeaconConfig> imple
                 beaconMap.put(url, beacon);
             }
         }
-        // cut list to closest 3 beacons if too large, might be a better way to deal with this...
-        if (closestBeacons.size() > 3) {
-            closestBeacons = closestBeacons.subList(0, 2);
-        }
-        if (closestBeacon != null) {
-            locOutput.sendMeasurement(closestBeacon);
-        }
-        determineLocation();
+
+        Beacon[] bArr = beaconMap.values().toArray(new Beacon[0]);
+        if (bArr.length >= 3)
+            locOutput.sendMeasurement(determineLocation(), bArr);   // Put better checks in place after testing
     }
 
     private List<Beacon> getBestBeacons(Collection<Beacon> beacons) {
@@ -211,34 +186,22 @@ public class BLEBeaconDriver extends AbstractSensorModule<BLEBeaconConfig> imple
         return bestBeacons;
     }
 
-    private void determineLocation() {
+    private double[] determineLocation() {
         /*if (closestBeacons.isEmpty()) {
             return;
         } else if (closestBeacons.size() < 3) {
-            closestBeacons.get(0).getDistance();
-            // TODO: figure out location here instead of in output...
-        } else {
-            // TODO: triangulate based on best three
-        }*/
-        if(beaconMap.size() == 3){
+            closestBeacons.get(0).getDistance();*/
+
+        if (beaconMap.size() == 3) {
             double[] distances = new double[3];
-//            double[][] locations = new double[3][3];
             Vect3d[] locations = new Vect3d[3];
-//            Vect3d[] enuLocations = new Vect3d[3];
             double[][] locationArr = new double[3][3];
-
+            GeoTransforms geoTransforms = new GeoTransforms();
             int i = 0;
-            for(Beacon beacon: beaconMap.values()){
+            for (Beacon beacon : beaconMap.values()) {
                 locations[i] = url2Locations.get(UrlBeaconUrlCompressor.uncompress(beacon.getId1().toByteArray()));
-                GeoTransforms geoTransforms = new GeoTransforms();
-//                NadirPointing nadirPointing = new NadirPointing();
-
                 Vect3d tempVec = new Vect3d();
                 geoTransforms.LLAtoECEF(locations[i], tempVec);
-               /* Mat3d rotMatrix = new Mat3d();
-                nadirPointing.getRotationMatrixENUToECEF(tempVec, rotMatrix);
-                rotMatrix.transpose();
-                enuLocations[i] = MatrixVectorProduct(rotMatrix, tempVec);*/
 
                 locations[i] = tempVec;
                 locationArr[i] = new double[]{locations[i].y, locations[i].x, locations[i].z};
@@ -246,31 +209,28 @@ public class BLEBeaconDriver extends AbstractSensorModule<BLEBeaconConfig> imple
                 i++;
             }
 
-            /*Triangulation trilaterationAlgo = new Triangulation();
-            Triangulation.Vec3d[] v3Loc = new Triangulation.Vec3d[4];
-            v3Loc[0] = new Triangulation.Vec3d(locations[0][1], locations[0][0], locations[0][2]);
-            v3Loc[1] = new Triangulation.Vec3d(locations[1][1], locations[1][0], locations[1][2]);
-            v3Loc[2] = new Triangulation.Vec3d(locations[2][1], locations[2][0], locations[2][2]);
-            v3Loc[3] = new Triangulation.Vec3d(locations[0][1], locations[0][0], locations[0][2]);
-            double[] triDist = {distances[0], distances[1], distances[2], distances[0]};
-
-            Triangulation.Vec3d solution = new Triangulation.Vec3d(0.0, 0.0, 0.0);
-            Log.d(TAG, "determineLocation: " + trilaterationAlgo.getLocation(solution, 0, v3Loc, triDist));*/
-
             double[] estLocation = AdaptedCellTrilateration.trackPhone(locationArr, distances);
+            Vect3d estECEF = new Vect3d(estLocation[0], estLocation[1], locations[0].z);
+            Vect3d estLLA = new Vect3d();
+            geoTransforms.ECEFtoLLA(estECEF, estLLA);
             Log.d(TAG, "determineLocation: " + estLocation[0] + "," + estLocation[1]);
-
-            Vect3d vector = new Vect3d();
+            return new double[]{estLLA.y * 180 / Math.PI, estLLA.x * 180 / Math.PI, estLLA.z};
         }
+        return new double[]{0, 0, 0}; // Todo: add better handling of this case
     }
-    
-    private Vect3d MatrixVectorProduct(Mat3d mat, Vect3d vec){
+
+    private Vect3d MatrixVectorProduct(Mat3d mat, Vect3d vec) {
         Vect3d resultVector = new Vect3d();
 
         resultVector.x = mat.m00 * vec.x + mat.m01 * vec.x + mat.m02 * vec.x;
         resultVector.y = mat.m10 * vec.y + mat.m11 * vec.y + mat.m12 * vec.y;
         resultVector.z = mat.m20 * vec.z + mat.m21 * vec.z + mat.m22 * vec.z;
-        
+
         return resultVector;
+    }
+
+    public double[] getBeaconLocation(Beacon beacon) {
+        Vect3d locationVec = url2Locations.get(UrlBeaconUrlCompressor.uncompress(beacon.getId1().toByteArray()));
+        return new double[]{locationVec.y * 180 / Math.PI, locationVec.x * 180 / Math.PI, locationVec.z};
     }
 }
--- sensorhub-android-blebeacon/src/main/java/org/sensorhub/impl/sensor/blebeacon/BLEBeaconLocationOutput.java
+++ sensorhub-android-blebeacon/src/main/java/org/sensorhub/impl/sensor/blebeacon/BLEBeaconLocationOutput.java
@@ -14,6 +14,8 @@ import org.sensorhub.impl.sensor.AbstractSensorOutput;
 import org.vast.swe.helper.GeoPosHelper;
 
 public class BLEBeaconLocationOutput extends AbstractSensorOutput<BLEBeaconDriver> {
+    private static final String URL_DEF = "http://sensorml.com/ont/swe/property/";
+
     String name;
     DataComponent posDataStruct;
     DataEncoding posEncoding;
@@ -33,7 +35,13 @@ public class BLEBeaconLocationOutput extends AbstractSensorOutput<BLEBeaconDrive
         vec.setLocalFrame(parentSensor.localFrameURI);
 
         posDataStruct.addComponent("time", fac.newTimeStampIsoUTC());
-        posDataStruct.addComponent("location", vec);
+        posDataStruct.addComponent("Estimated Location", vec);
+        posDataStruct.addComponent("Beacon 1 Location", vec);
+        posDataStruct.addComponent("Beacon 1 Range", fac.newQuantity(URL_DEF + "distance", null, null, "m"));
+        posDataStruct.addComponent("Beacon 2 Location", vec);
+        posDataStruct.addComponent("Beacon 2 Range", fac.newQuantity(URL_DEF + "distance", null, null, "m"));
+        posDataStruct.addComponent("Beacon 3 Location", vec);
+        posDataStruct.addComponent("Beacon 3 Range", fac.newQuantity(URL_DEF + "distance", null, null, "m"));
 
         // output encoding
         posEncoding = fac.newTextEncoding(",", "\n");
@@ -61,26 +69,33 @@ public class BLEBeaconLocationOutput extends AbstractSensorOutput<BLEBeaconDrive
         return 0;
     }
 
-    protected void sendMeasurement(Beacon beacon) {
+    protected void sendMeasurement(double[] estLocation, Beacon[] beacons) {
         DataBlock dataBlock = posDataStruct.createDataBlock();
         double sampleTime = System.currentTimeMillis()/1000;
+        double[] b1Loc = parentSensor.getBeaconLocation(beacons[0]);
+        double[] b2Loc = parentSensor.getBeaconLocation(beacons[1]);
+        double[] b3Loc = parentSensor.getBeaconLocation(beacons[2]);
 
-        if (UrlBeaconUrlCompressor.uncompress(beacon.getId1().toByteArray()).equals("http://cardbeacon")) {
-            dataBlock.setDoubleValue(0, sampleTime);
-            dataBlock.setDoubleValue(1, 34.2520745);
-            dataBlock.setDoubleValue(2, -86.2012021);
-            dataBlock.setDoubleValue(3, 283.0);
-        } else if (UrlBeaconUrlCompressor.uncompress(beacon.getId1().toByteArray()).equals("http://opensensorhub.org")){
-            dataBlock.setDoubleValue(0, sampleTime);
-            dataBlock.setDoubleValue(1, 34.251943);
-            dataBlock.setDoubleValue(2, -86.201209);
-            dataBlock.setDoubleValue(3, 283.0);
-        }else{
-            dataBlock.setDoubleValue(0, sampleTime);
-            dataBlock.setDoubleValue(1, 0.0);
-            dataBlock.setDoubleValue(2, 0.0);
-            dataBlock.setDoubleValue(3, 0.0);
-        }
+        dataBlock.setDoubleValue(0, sampleTime);
+        dataBlock.setDoubleValue(1, estLocation[0]);
+        dataBlock.setDoubleValue(2, estLocation[1]);
+        dataBlock.setDoubleValue(3, estLocation[2]);
+
+        dataBlock.setDoubleValue(4, b1Loc[0]);
+        dataBlock.setDoubleValue(5, b1Loc[1]);
+        dataBlock.setDoubleValue(6, b1Loc[2]);
+        dataBlock.setDoubleValue(7, beacons[0].getDistance());
+
+        dataBlock.setDoubleValue(8, b2Loc[0]);
+        dataBlock.setDoubleValue(9, b2Loc[1]);
+        dataBlock.setDoubleValue(10, b2Loc[2]);
+        dataBlock.setDoubleValue(11, beacons[1].getDistance());
+
+        dataBlock.setDoubleValue(12, b3Loc[0]);
+        dataBlock.setDoubleValue(13, b3Loc[1]);
+        dataBlock.setDoubleValue(14, b3Loc[2]);
+        dataBlock.setDoubleValue(15, beacons[2].getDistance());
+        
         // update latest record and send event
         latestRecordTime = System.currentTimeMillis();
         eventHandler.publishEvent(new SensorDataEvent(latestRecordTime, BLEBeaconLocationOutput.this, dataBlock));
